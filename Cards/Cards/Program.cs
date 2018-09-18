9a1043c75aab05450d45e04d461006becd7114ab
Testo12 <erik.malmsten99@gmail.com>
2018-09-18 16:26:40 +0200

    makes card, duplicates

--- Cards/Cards/Program.cs
+++ Cards/Cards/Program.cs
@@ -10,6 +10,101 @@ namespace Cards
     {
         static void Main(string[] args)
         {
+
+            int active = 0;
+
+            do
+            {
+
+
+                Console.WriteLine("Vilket programm? A,B, eller C");
+                String choice = Console.ReadLine();
+                String Change = choice.ToLower();
+                switch (Change)
+                {
+
+
+                    case "a":
+
+                        var Cards = new string[52];
+                        int i = 0;
+                        string color = "";
+                        Random value = new Random();
+                        
+
+                        foreach(var kort in Cards)
+                        {
+                            int suit = value.Next(1, 5);
+                            int number = value.Next(1, 13);
+
+                            if (suit == 1)
+                            {
+                            color = "Klöver ";
+                            }
+
+                        else if (suit == 2)
+                            {
+                            color = "Hjärter ";
+                            }
+
+                        else if (suit == 3)
+                            {
+                            color = "Spader ";
+                        }
+                        else if (suit == 4)
+                            {
+                            color = "Diamant ";
+                        }
+                            string done = number + " " + color;
+                            string val = number.ToString();
+                            string compare = val + color;
+
+                            int i = 0;
+                            Console.WriteLine("{i}", kort);
+                            //Console.WriteLine("Ditt kort är " + done );
+                            i++;
+
+                        }
+
+
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        active = 1;
+                break;
+
+                    case "b:":
+
+                        active = 1;
+                break;
+
+                    case "c":
+
+
+
+                        active = 1;
+                break;
+
+                }
+
+            }
+            while (active != 0);
         }
-    }
+            
+        }
+
 }
